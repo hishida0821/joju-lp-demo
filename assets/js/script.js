6c6142e290835ba38f1552f3971ad8a720d9a9cb
hiroki ishida <hiroki.ishida@acrospera.com>
2026-04-13 14:52:59 +0900

    fix script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@
 
 function handleCheckSubscription (){
   const days = window.__APP_DAYS_SINCE_SUBSCRIPTION__;
-  const borderLine = 3;
+  const borderLine = 5;
   const button = document.querySelector('js-cta_button');
 
   alert('days =' + days)
 
   // console.log("daysSinceSubscription:", days);
   if(days <= borderLine) {
-    // button.style.cssText = 'pointer-events: none;';
+    button.style.cssText = 'pointer-events: none;';
     alert('サブスク期間が足りていません')
   } else {
     alert('サブスク期間が足りています')
