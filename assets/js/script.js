e317de20f5eab9d4ccec0f05eb954a3298653689
hiroki ishida <hiroki.ishida@acrospera.com>
2026-04-14 09:42:09 +0900

    fix get query

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,29 @@
 function handleCheckSubscription (){
   const days = window.__APP_DAYS_SINCE_SUBSCRIPTION__;
   const availableDate = window.__APP_AVAILABLE_DATE__;
-  const button = document.querySelector('js-cta_button');
+  const linkButton = document.querySelector('js-cta_button.--link');
+  const popupButton = document.querySelector('js-cta_button.--popup');
 
   alert(`サブスク期間は${days}で、期限は${availableDate}です。`)
 
   // console.log("daysSinceSubscription:", days);
   if(days <= availableDate) {
-    button.style.cssText = 'pointer-events: none;';
+    popupButton.style.cssText = "opacity: 1; visibility: visible";
     alert('サブスク期間が足りていません')
   } else {
+    linkButton.style.cssText = "opacity: 1; visibility: visible";
     alert('サブスク期間が足りています')
   }
 
 }
-window.addEventListener('DOMContentLoaded', handleCheckSubscription, false);
+// window.addEventListener('DOMContentLoaded', handleCheckSubscription, false);
+
+function HandleGetQuery(){
+  const params = new URLSearchParams(window.location.search);
+  const userId = params.get('user_id'); // "123"
+  const startedAt = params.get('subscription_started_at'); // "2026-04-01"
+
+  alert(`ユーザーIDは${userId}で、サブスク期間は${startedAt}です。`)
+
+}
+window.addEventListener('DOMContentLoaded', HandleGetQuery, false);
--- cp02.php
+++ cp02.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="robots" content="noindex, nofollow">
   <title>美活チャレンジキャンペーン開催！</title>
   <link rel="stylesheet" href="./assets/css/style.css">
   <link rel="stylesheet" href="./assets/css/campaign.css">
