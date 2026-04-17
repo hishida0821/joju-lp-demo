52517a2d2298aaab6ccf4e51ed9d6442b7dc4914
hiroki ishida <hiroki.ishida@acrospera.com>
2026-04-17 18:04:15 +0900

    implement get query, popup script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,25 @@
 'use strict';
 
-// function handleCheckSubscription (){
-//   const days = window.__APP_DAYS_SINCE_SUBSCRIPTION__;
-//   const availableDate = window.__APP_AVAILABLE_DATE__;
-//   const linkButton = document.querySelector('js-cta_button.--link');
-//   const popupButton = document.querySelector('js-cta_button.--popup');
-
-//   alert(`サブスク期間は${days}で、期限は${availableDate}です。`)
-
-//   // console.log("daysSinceSubscription:", days);
-//   if(days <= availableDate) {
-//     popupButton.style.cssText = "opacity: 1; visibility: visible";
-//     alert('サブスク期間が足りていません')
-//   } else {
-//     linkButton.style.cssText = "opacity: 1; visibility: visible";
-//     alert('サブスク期間が足りています')
-//   }
-
-// }
-// window.addEventListener('DOMContentLoaded', handleCheckSubscription, false);
-
-
 function handlePopup(){
   const html = document.querySelector('html');
   const popup = document.querySelector('.js-popup');
+  const closeButton = document.querySelector('.js-popup_close');
 
   html.style.cssText = 'overflow: hidden';
   popup.style.cssText = 'opacity: 1; visibility: visible';
+
+  if(closeButton) {
+    closeButton.addEventListener('click', function(){
+      html.style.cssText = ''
+      popup.style.cssText = ''
+    })
+  }
 }
 
 function handleGetQuery(){
   const params = new URLSearchParams(window.location.search);
   const userId = params.get('user_id');
-  // const startedAt = params.get('subscription_started_at');
-  const startedAt = '2026-04-01'
+  const startedAt = params.get('subscription_started_at');
 
   const toDayObj = new Date();
   const toDay = `${toDayObj.getFullYear()}-${String(toDayObj.getMonth() + 1).padStart(2, '0')}-${String(toDayObj.getDate()).padStart(2, '0')}`;
@@ -47,7 +33,7 @@ function handleGetQuery(){
     const diffMs = toDayObj.setHours(0,0,0,0) - startedAtObj.setHours(0,0,0,0);
     const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
     // console.log(`開始日からの経過日数: ${diffDays}日`);
-    if (diffDays >= 4) {
+    if (diffDays >= 100) {
       if (linkButton) {
         linkButton.style.cssText = "display: block;";
       }
